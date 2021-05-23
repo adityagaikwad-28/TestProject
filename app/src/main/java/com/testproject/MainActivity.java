6908e08fd13b7a03cdece0f4c884c28bbb687431
adityagaikwad-28 <ag733385@gmail.com>
2021-05-23 23:42:47 +0530

    Initial commit

--- app/src/main/java/com/testproject/MainActivity.java
+++ app/src/main/java/com/testproject/MainActivity.java
@@ -2,13 +2,31 @@ package com.testproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView img,img1;
+    TextView tv1,tv2,tv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img=(ImageView) findViewById(R.id.img);
+        img1=(ImageView) findViewById(R.id.img1);
+        tv1=(TextView) findViewById(R.id.tv1);
+        tv2=(TextView) findViewById(R.id.tv2);
+        tv3=(TextView) findViewById(R.id.tv3);
+        img.setOnClickListener(View v){
+            Intent inl=new Intent(MainActivity.this,);
+        }
+
     }
 }
